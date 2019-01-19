3dafdadc4221754e6addf9d56f8bf9d3cf6a1aa1
Valencia Serrao <serraovalencia@gmail.com>
2019-01-19 11:39:33 +0000

    Terraform hello-world updated

---
+++ resources.tf
@@ -0,0 +1,13 @@
+resource "google_compute_network" "our_development_network" {
+	name = "devnetwork"
+	auto_create_subnetworks = true
+}
+
+resource "aws_vpc" "environment-example-three" {
+	cidr_block = "10.0.0.0/16"
+	enable_dns_hostnames = true
+	enable_dns_support = true
+	tags {
+		Name = "terraform-aws-vpc-example-three"
+	}
+}
